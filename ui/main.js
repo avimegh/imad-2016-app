c901b3e01aeaeade9677bffacb136b8487bf632b
avimegh <avinashyagnapu@gmail.com>
2016-09-27 00:58:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,11 @@
-console.log('Loaded!');
-if (typeof element !== 'undefined') {
-  // Now we know that element is defined, we are good to go.
-}
-console.log('avi');
-var element=document.getElementById('main-text');  
-
-element.innerHTML='new value';
-//move image
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft-marginLeft + 'px';
-}
-img.onclick=function(){
-    var inteval = setInterval(moveRight, 50);
+//counter code
+var button=document.getElementsById('counter');
+var counter=0;
+button.onclick=function(){
+    //make a request to the counter endpoint
+    //capture the response  and store  it  in a variable
+    //render the variable in the correct span
+    counter=counter + 1;
+    var span=document.getElementsById('count');
+    span.innerHTML=counter.toString();
 };

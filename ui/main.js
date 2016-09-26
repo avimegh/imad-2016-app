bf116cf73af0e27086449b824d0de8d9b6e6d180
avimegh <avinashyagnapu@gmail.com>
2016-09-27 00:29:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,9 @@ console.log('avi');
 var element=document.getElementById('main-text');  
 
 element.innerHTML='new value';
+//move image
+
 var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft-marginLeft + 'px';
-}
 img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+    img.style.marginLet='100px';
 };
